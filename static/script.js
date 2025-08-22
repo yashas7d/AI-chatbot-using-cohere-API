6a29e707e45b71a749ed0d3f62874f7e3a8cc22e
yashas7d <138746367+yashas7d@users.noreply.github.com>
2025-08-22 20:02:10 +0530

    css and js file uploaded

---
+++ static/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", () => {
+        console.log("Form submitted!");
+    });
+});
